eeb18fd00ba9b58f22d504014d550d2477ee687e
Lucas Colwell <colwellc37@gmail.com>
2022-09-26 11:04:58 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # resources :patients
   resources :doctors, only: [:show] do
     resources :doctor_patients, only: [:destroy]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :patients, only: [:index]
 end
